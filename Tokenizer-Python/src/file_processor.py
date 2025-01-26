551f7f5271035a4e530536975c5f7677e2cddcff
reynaroyce12 <reynaroyce.12@gmail.com>
2025-01-25 20:53:04 -0600

    Added changes to measure only the tokenization time

--- Tokenizer-Python/src/file_processor.py
+++ Tokenizer-Python/src/file_processor.py
@@ -1,7 +1,11 @@
 import os 
+import time
 from tokenizer import Tokenize
+import pprint
 
 def process_directories(input_folder, output_folder="output"):
+    file_contents = {}
+
     for dirpath, dirname, files in os.walk(input_folder):
         relative_path = os.path.relpath(dirpath, input_folder)
         output_path = os.path.join(output_folder, relative_path)
@@ -9,14 +13,20 @@ def process_directories(input_folder, output_folder="output"):
 
         for file in files:
             input_file = os.path.join(dirpath, file)
-            output_file = os.path.join(output_path, file)
-
             with open(input_file, 'r', encoding='utf-8') as source_file:
-                content = source_file.read()
+                file_contents[input_file] = source_file.read()
 
-            tokens = Tokenize(content)
+    start_time = time.time()
+    tokenized_output = {file: Tokenize(content) for file, content in file_contents.items()}
+    end_time = time.time()
 
-            with open(output_file, 'w', encoding='utf-8') as destination_file:
-                destination_file.write(' '.join(tokens))
+    elapsed_time = round(end_time - start_time, 2)
 
+    for file, tokens in tokenized_output.items():
+        relative_path = os.path.relpath(file, input_folder)
+        output_file = os.path.join(output_folder, relative_path)
 
+        with open(output_file, 'w', encoding='utf-8') as destination_file:
+            destination_file.write(' '.join(tokens))
+
+    return elapsed_time
--- Tokenizer-Python/src/main.py
+++ Tokenizer-Python/src/main.py
@@ -1,23 +1,19 @@
 import os
 import sys
-import time
+
 from file_processor import process_directories
 
 def main():
 
-    input_folder = "/Users/reynaroyce/Documents/vava/Development/Celestis-Tokenizers/dataset3"
+    input_folder = "/Users/reynaroyce/Documents/vava/Development/Celestis-Tokenizers/datasets/dataset1"
     output_folder = "/Users/reynaroyce/Documents/vava/Development/Celestis-Tokenizers/Tokenizer-Python/output"
 
     if not os.path.exists(input_folder):
         print(f"Input directory {input_folder} does not exist!")
         sys.exit(1)
 
-    start_time = time.time()
+    elapsed_time = process_directories(input_folder, output_folder)
 
-    process_directories(input_folder, output_folder)
-
-    end_time = time.time()
-    elapsed_time = round(end_time - start_time, 2)
     print(f"Execution time: {elapsed_time} seconds")
 
 
